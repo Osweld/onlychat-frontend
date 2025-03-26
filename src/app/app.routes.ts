f92270aa3bec03a9d4599c0d6296dc614f4088ca
German Reyes <oswaldo101.gr@gmail.com>
2025-03-25 20:18:16 -0600

    Add authentication routes for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...authRoutes
+
+    
+];
---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,7 @@
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { Routes } from '@angular/router';
+
+
+export const authRoutes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+];
