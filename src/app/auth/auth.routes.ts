2a6cff2ddaedc2353aad415f2a0f92606984c8fb
German Reyes <oswaldo101.gr@gmail.com>
2025-03-27 16:50:03 -0600

    Add registration route to auth module

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 export const authRoutes: Routes = [
   {path: 'login', component: LoginPageComponent},
+ {path: 'register', component: RegisterPageComponent},
 ];
