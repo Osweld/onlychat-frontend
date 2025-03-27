f68075ae0e4a205384ba1c85e7cdf2739f162ed2
German Reyes <oswaldo101.gr@gmail.com>
2025-03-27 10:07:14 -0600

    Add AuthService for handling login and password reset functionality

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { LoginCredentials, LoginResponse } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  http = inject(HttpClient);
+
+  URL:String = environment.api.baseUrl + '/v1/auth';
+
+  constructor() { }
+
+  login(loginCredentials: LoginCredentials) {
+    const { username, password } = loginCredentials;
+    return this.http.post<LoginResponse>(this.URL + '/login', {username, password});
+  }
+
+  forgotPassword(email: string) {
+    return this.http.get(this.URL + '/reset-password', {params: {email}});
+  }
+}
