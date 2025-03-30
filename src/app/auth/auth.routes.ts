a7792485903febe5e5bcccb243440e0e2cf1f3fd
German Reyes <oswaldo101.gr@gmail.com>
2025-03-29 20:27:12 -0600

    Add routing for account activation and set default redirect to login

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,13 @@
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { AccountActivationPageComponent } from './pages/account-activation-page/account-activation-page.component';
 
 
 export const authRoutes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginPageComponent},
  {path: 'register', component: RegisterPageComponent},
+ {path: 'activate/:token', component: AccountActivationPageComponent},
+ {path: '**', redirectTo: 'login'}
 ];
