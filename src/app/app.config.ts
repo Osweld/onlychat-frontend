5efd5c31dffbc14b3b774b5ce5bbe37f4dc16622
German Reyes <oswaldo101.gr@gmail.com>
2025-03-29 20:25:48 -0600

    Refactor app configuration to integrate JWT module and enhance router setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 import { providePrimeNG } from 'primeng/config';
+import { environment } from '../environments/environment.development';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
@@ -9,28 +15,41 @@ import { provideHttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 
 export function tokenGetter() {
-  return localStorage.getItem("access_token");
+  return localStorage.getItem('access_token');
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: [environment.api.baseUrl],
+          disallowedRoutes: [
+            `${environment.api.baseUrl}/api/v1/auth/login`,
+            `${environment.api.baseUrl}/api/v1/auth/signup`,
+            `${environment.api.baseUrl}/api/v1/auth/activate-account`,
+            `${environment.api.baseUrl}/api/v1/auth/resend-activate-account-token`,
+            `${environment.api.baseUrl}/api/v1/auth/reset-password`,
+          ],
+        },
+      })
+    ),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-          preset: Aura,
-          options: {
-            cssLayer: {
-                name: 'primeng',
-                order: 'theme, base, primeng'
-            }
-        }
-          
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
       },
-      
-  }),
-  MessageService
+    }),
+    MessageService,
   ],
 };
